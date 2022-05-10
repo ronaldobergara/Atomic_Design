939ec7a0b38d9456c6bdc75f01f52d6bb6f48304
ronaldobergara <bergaraheadbanger@gmail.com>
2022-05-10 07:52:39 -0300

    Configuração projeto,  criando primeiro atom

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <Logo />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import { Logo } from "@/components/atoms"
+
+export default defineComponent({
+  components: { Logo }
+})
+</script>
+
+<style scoped>
+.component {
+  width: 300px;
+}
+</style>
